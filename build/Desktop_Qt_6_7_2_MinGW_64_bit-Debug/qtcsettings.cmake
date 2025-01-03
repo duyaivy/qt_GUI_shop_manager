b8f2a0cca2fa570da45cbaea7c7008de95e5bf14
unknown <quocduy0322@gmail.com>
2025-01-03 23:07:48 +0700

    deploy: shop_manager.exe

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/qtcsettings.cmake
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -1,3 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/QUOCDUY/qt_pbl2/qt_GUI_shop_manager/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
